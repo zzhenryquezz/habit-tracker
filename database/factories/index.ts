c0f60f7fd5fc30fd9bd6ec6b2ecf4d5d422688bc
Henrique <henryqueoli@hotmail.com>
2022-02-18 12:13:08 -0300

    test: add HabitsController integrations tests

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,4 +1,5 @@
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import Habit from 'App/Models/Habit'
 import User from 'App/Models/User'
 
 export const UserFactory = Factory.define(User, ({ faker }) => {
@@ -6,5 +7,13 @@ export const UserFactory = Factory.define(User, ({ faker }) => {
     name: faker.name.findName(),
     email: faker.internet.email(),
     password: faker.internet.password(),
+  }
+})
+  .relation('habits', () => HabitFactory)
+  .build()
+
+export const HabitFactory = Factory.define(Habit, ({ faker }) => {
+  return {
+    name: faker.name.findName(),
   }
 }).build()
---
+++ test/controllers/HabitsController.int.spec.ts
@@ -0,0 +1,184 @@
+import test from 'japa'
+import Habit from 'App/Models/Habit'
+import { UserFactory } from 'Database/factories'
+import { createClient } from 'Test/fixtures/client'
+
+test.group('HabitsController (int)', (group) => {
+  const client = createClient()
+
+  group.afterEach(async () => {
+    await client.logout()
+    await Habit.query().delete()
+  })
+
+  test('[index] should admin user get habits of other user', async (assert) => {
+    await client.login()
+
+    const user = await UserFactory.with('habits', 5).create()
+
+    const { body } = await client.get(`/api/users/${user.id}/habits`)
+
+    assert.equal(body.data.length, 5)
+  })
+
+  test('[index] should normal user not get habits of other user', async () => {
+    await client.login({ isAdmin: false })
+
+    const user = await UserFactory.with('habits', 5).create()
+
+    await client.get(`/api/users/${user.id}/habits`).expect(403)
+  })
+
+  test('[index] should normal get habits of himself', async (assert) => {
+    const { email, id } = await UserFactory.with('habits', 5).create()
+
+    await client.login({ email: email, isAdmin: false })
+
+    const { body } = await client.get(`/api/users/${id}/habits`).expect(200)
+
+    assert.equal(body.data.length, 5)
+  })
+
+  test('[show] should admin user get a habit of other user', async (assert) => {
+    await client.login()
+
+    const { habits } = await UserFactory.with('habits', 1).create()
+
+    const { body } = await client.get(`/api/users/${habits[0].userId}/habits/${habits[0].id}`)
+
+    assert.equal(body.id, habits[0].id)
+  })
+
+  test('[show] should normal user not get a habit of other user', async () => {
+    await client.login({ isAdmin: false })
+
+    const { habits } = await UserFactory.with('habits', 1).create()
+
+    await client.get(`/api/users/${habits[0].userId}/habits/${habits[0].id}`).expect(403)
+  })
+
+  test('[show] should normal user get a habit of himself', async () => {
+    const { habits, email } = await UserFactory.with('habits', 1).create()
+
+    await client.login({ email, isAdmin: false })
+
+    await client.get(`/api/users/${habits[0].userId}/habits/${habits[0].id}`).expect(200)
+  })
+
+  test('[store] should admin user create a habit of other user', async (assert) => {
+    await client.login()
+
+    const { id } = await UserFactory.create()
+
+    const { body } = await client.post(`/api/users/${id}/habits`).send({
+      name: 'test',
+    })
+
+    assert.equal(body.name, 'test')
+  })
+
+  test('[store] should normal user not create a habit of other user', async (assert) => {
+    await client.login({ isAdmin: false })
+
+    const { id } = await UserFactory.create()
+
+    await client.post(`/api/users/${id}/habits`).expect(403)
+  })
+
+  test('[store] should normal user create a habit of himself', async (assert) => {
+    const { id, email } = await UserFactory.create()
+
+    await client.login({ isAdmin: false, email })
+
+    const { body } = await client
+      .post(`/api/users/${id}/habits`)
+      .send({ name: 'teste' })
+      .expect(200)
+
+    assert.equal(body.name, 'teste')
+  })
+
+  test('[update] should admin user update a habit of other user', async (assert) => {
+    await client.login()
+
+    const user = await UserFactory.with('habits', 1).create()
+    const habit = user.habits[0]
+
+    await client
+      .patch(`/api/users/${user.id}/habits/${habit.id}`)
+      .send({ name: 'teste-update' })
+      .expect(200)
+
+    await habit.refresh()
+
+    assert.equal(habit.name, 'teste-update')
+  })
+
+  test('[update] should normal user not update a habit of other user', async () => {
+    await client.login({ isAdmin: false })
+
+    const user = await UserFactory.with('habits', 1).create()
+    const habit = user.habits[0]
+
+    await client
+      .patch(`/api/users/${user.id}/habits/${habit.id}`)
+      .send({ name: 'teste-update' })
+      .expect(403)
+  })
+
+  test('[update] should normal user update a habit of himself', async (assert) => {
+    const user = await UserFactory.with('habits', 1).create()
+    const habit = user.habits[0]
+
+    await client.login({ email: user.email, isAdmin: false })
+
+    await client
+      .patch(`/api/users/${user.id}/habits/${habit.id}`)
+      .send({ name: 'teste-update' })
+      .expect(200)
+
+    await habit.refresh()
+
+    assert.equal(habit.name, 'teste-update')
+  })
+
+  test('[destroy] should admin user delete a habit of other user', async (assert) => {
+    await client.login()
+
+    const user = await UserFactory.with('habits', 1).create()
+    const habit = user.habits[0]
+
+    await client.delete(`/api/users/${user.id}/habits/${habit.id}`).expect(200)
+
+    const exist = await Habit.find(habit.id)
+
+    assert.isNull(exist)
+  })
+
+  test('[destroy] should normal user not delete a habit of other user', async (assert) => {
+    await client.login({ isAdmin: false })
+
+    const user = await UserFactory.with('habits', 1).create()
+    const habit = user.habits[0]
+
+    await client.delete(`/api/users/${user.id}/habits/${habit.id}`).expect(403)
+
+    const exist = await Habit.find(habit.id)
+
+    assert.isNotNull(exist)
+  })
+
+  test('[destroy] should normal user delete a habit of himself', async (assert) => {
+    const user = await UserFactory.with('habits', 1).create()
+
+    await client.login({ email: user.email, isAdmin: false })
+
+    const habit = user.habits[0]
+
+    await client.delete(`/api/users/${user.id}/habits/${habit.id}`).expect(200)
+
+    const exist = await Habit.find(habit.id)
+
+    assert.isNull(exist)
+  })
+})
