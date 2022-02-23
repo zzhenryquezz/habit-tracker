fcc7b6827fceb27820f1d53f17188db8bd926596
Henrique <henryqueoli@hotmail.com>
2022-02-23 14:45:41 -0300

    refactor: update dashboard dist path

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,8 +1,28 @@
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
 
-const config = defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  const { API_PROXY_URL } = loadEnv(mode, process.cwd())
+
+  const proxyUrl = API_PROXY_URL || 'http://localhost:3333'
+
+  return {
+    build: {
+      outDir: path.resolve(__dirname, '..', './public'),
+    },
+    plugins: [vue()],
+    resolve: {
+      // alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    server: {
+      proxy: {
+        '^/api/.*': {
+          target: `${proxyUrl}/api`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
-
-export default config
