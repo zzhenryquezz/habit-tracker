197f69f78efb5c25697402d7275bf54f26249019
Henrique <henryqueoli@hotmail.com>
2022-02-23 12:57:25 -0300

    docs: add users-controller requests

--- app/Validators/UpdateUserValidator.ts
+++ app/Validators/UpdateUserValidator.ts
@@ -1,5 +1,4 @@
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class UpdateUserValidator {
   public schema = schema.create({
