1bc028741e3153e6f28a1547b49ccb466c98dcbc
Henrique <henryqueoli@hotmail.com>
2022-02-23 13:35:54 -0300

    docs: add habit-sequence requests

--- app/Models/HabitSequence.ts
+++ app/Models/HabitSequence.ts
@@ -14,9 +14,9 @@ export default class HabitSequence extends BaseModel {
   @column()
   public done: boolean
 
-  @column.dateTime({ autoCreate: true })
+  @column.dateTime({ autoCreate: true, serializeAs: null })
   public createdAt: DateTime
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null })
   public updatedAt: DateTime
 }
--- app/Validators/HabitSequenceStoreValidator.ts
+++ app/Validators/HabitSequenceStoreValidator.ts
@@ -2,6 +2,7 @@ import { schema } from '@ioc:Adonis/Core/Validator'
 
 export default class HabitSequenceStoreValidator {
   public schema = schema.create({
+    done: schema.boolean.optional(),
     date: schema.date({
       format: 'yyyy-MM-dd',
     }),
