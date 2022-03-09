44fe71aa58b51d048608be8fe6804e35a6f4688e
Henrique <henryqueoli@hotmail.com>
2022-03-09 12:52:00 -0300

    feat(dash): add roboto font

--- dashboard/App.vue
+++ dashboard/App.vue
@@ -34,3 +34,11 @@ function getLayout(component: any) {
     </component>
   </router-view>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500&display=swap');
+
+#app {
+  font-family: 'Roboto', sans-serif;
+}
+</style>
