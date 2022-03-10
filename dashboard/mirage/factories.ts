78d3e0c80ee2d99b8b9fbb32b4455a0a815d5d29
Henrique <henryqueoli@hotmail.com>
2022-03-10 17:38:50 -0300

    fix: add models and factories to mirage server

--- dashboard/mirage/factories.ts
+++ dashboard/mirage/factories.ts
@@ -1,9 +1,8 @@
-import faker from 'faker'
 import { Factory } from 'miragejs'
 
 const userFactory = Factory.extend({
-  name: () => faker.name.findName(),
-  email: () => faker.internet.email(),
+  name: (i) => `User ${i}`,
+  email: (i) => `user-${i}@test.com`,
 })
 
 export default {
--- dashboard/mirage/index.ts
+++ dashboard/mirage/index.ts
@@ -1,13 +1,19 @@
 import { createServer as baseCreateServer } from 'miragejs'
 import AuthController from './controllers/auth-controller'
+import models from './models'
+import factories from './factories'
 
 export function createServer({ environment = 'development' } = {}) {
   return baseCreateServer({
     environment,
+    models,
+    factories,
     routes() {
       this.namespace = 'api'
 
       this.post('/auth/login', AuthController.login)
+
+      this.get('/auth/who-i-am', AuthController.whoIAm)
     },
   })
 }
