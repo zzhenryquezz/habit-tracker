eac580f00d71e2b043724f72a56137a79a2c54e2
Henrique <henryqueoli@hotmail.com>
2022-04-02 01:23:47 -0300

    feat(dash): add h-checkbox component

---
+++ dashboard/components/h-checkbox.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+})
+const emit = defineEmits(['update:modelValue'])
+
+const model = computed({
+  get: () => props.modelValue,
+  set: (v) => emit('update:modelValue', v),
+})
+</script>
+<template>
+  <input v-model="model" type="checkbox" class="hidden" />
+
+  <w-card
+    :color="model ? 'primary' : 'teal-100'"
+    width="6"
+    height="6"
+    class="cursor-pointer border"
+    @click="model = !model"
+  />
+</template>
